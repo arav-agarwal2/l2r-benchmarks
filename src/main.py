192b596b5a6e3278eb70faa15d39c5bd5654de7c
kchian <chian.kevin@gmail.com>
2022-08-30 13:44:31 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,17 +9,6 @@ from runners.sac import SACRunner
 This script uses the subprocess module to run the simulator.
 """
 
-
-def race_n_episodes(env: RacingEnv, runner: BaseRunner, num_episodes: int = 5):
-    """Complete an episode in the environment"""
-
-    for ep in range(num_episodes):
-        logging.info(f"Episode {ep+1} of {num_episodes}")
-
-        obs = env.reset()
-        runner.run()
-
-
 if __name__ == "__main__":
     # Build environment
     env = build_env()
@@ -27,4 +16,4 @@ if __name__ == "__main__":
     buffer = ReplayBuffer()
     runner = SACRunner(env, None, None, buffer)
     # Race!
-    race_n_episodes(env=env, runner=runner, num_episodes=1)
+    runner.run()
