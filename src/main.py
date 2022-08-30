ffefa8e1a95fbabced1587ed9389e95f04a8893c
kchian <chian.kevin@gmail.com>
2022-08-30 13:55:24 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ import logging
 import subprocess
 from l2r import build_env
 from l2r import RacingEnv
-# from buffers.replay_buffer import ReplayBuffer
-# from runners.sac import SACRunner
+from buffers.replay_buffer import ReplayBuffer
+from runners.sac import SACRunner
 
 """
 This script uses the subprocess module to run the simulator.
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     # Build environment
     env = build_env()
     
-#     buffer = ReplayBuffer()
-#     runner = SACRunner(env, None, None, buffer)
-#     # Race!
-#     runner.run()
+    buffer = ReplayBuffer()
+    runner = SACRunner(env, None, None, buffer)
+    # Race!
+    runner.run()
