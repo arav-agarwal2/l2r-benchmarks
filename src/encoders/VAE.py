613fdac675a4360b6b4aabc3218f17dc04264ac8
Arav Agarwal <arava@andrew.cmu.edu>
2022-08-31 11:09:48 -0400

    Halp13

--- src/encoders/VAE.py
+++ src/encoders/VAE.py
@@ -74,7 +74,10 @@ class VAE(BaseEncoder):
 
     def encode(self, x, device=DEVICE):
         x = torch.as_tensor(x, device=device, dtype=torch.float)
-        raise ValueError(x.shape)
+        if len(x.shape) == 3:
+            x = x.permute(2,0,1)
+        else:
+            x = x.permute(0,3,1,2)
         h = self.encoder(x)
         z, mu, logvar = self.bottleneck(h)
         return z, mu, logvar
