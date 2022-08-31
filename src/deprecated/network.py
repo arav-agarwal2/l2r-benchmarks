5f99b4f37d6d6e28f33aecd6ddf30a8a690dc842
Arav Agarwal <arava@andrew.cmu.edu>
2022-08-31 12:40:57 -0400

    Halp13

--- src/deprecated/network.py
+++ src/deprecated/network.py
@@ -96,7 +96,7 @@ class ActorCritic(nn.Module):
         #    obs_feat = obs_feat.unsqueeze(0)
         img_embed = obs_feat[..., :32] # n x latent_dims
         speed = obs_feat[..., 32:] # n x 1
-        print(obs_feat.shape, img_embed.shape, speed.shape)
+        raise ValueError(obs_feat.shape, img_embed.shape, speed.shape)
         spd_embed = self.speed_encoder(speed) # n x 8
         feat = torch.cat([img_embed, spd_embed], dim = -1)
         return self.policy(feat, deterministic, True)
