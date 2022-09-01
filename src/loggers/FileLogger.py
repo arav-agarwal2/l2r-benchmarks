a3a8cc28c5fe8161120ebaf319b799b0e02f7849
Arav Agarwal <arava@andrew.cmu.edu>
2022-09-01 08:03:19 -0400

    bugfix

--- src/loggers/FileLogger.py
+++ src/loggers/FileLogger.py
@@ -7,15 +7,15 @@ from datetime import datetime
 
 @yamlize
 class FileLogger(BaseLogger):
-    def __init__(self, log_dir:str, exp_name:str) -> None:
-        super().__init__(log_dir, exp_name)
+    def __init__(self, log_dir:str, experiment_name:str) -> None:
+        super().__init__(log_dir, experiment_name)
         now = datetime.now()
         self.log_dir = log_dir
         logging.basicConfig(
             level=logging.INFO,
             format="%(asctime)s [%(levelname)s] %(message)s",
             handlers=[
-                logging.FileHandler(f"{self.log_dir}/runlogs/{exp_name}.log"),
+                logging.FileHandler(f"{self.log_dir}/runlogs/{experiment_name}.log"),
                 logging.StreamHandler(sys.stdout),
             ],
             force=True
