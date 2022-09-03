be97b2d1edc9bbf54ffb1859c8e09d8de5bf105e
kchian <chian.kevin@gmail.com>
2022-09-03 19:10:23 -0400

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -3,14 +3,15 @@ import subprocess
 from l2r import build_env
 from l2r import RacingEnv
 from src.runners.sac import SACRunner
-
-"""
-This script uses the subprocess module to run the simulator.
-"""
+import sys
 
 if __name__ == "__main__":
     # Build environment
     env = build_env()
     runner = SACRunner(env)
+    
+    with open(f"{runner.agent.model_save_path}/{runner.exp_config["experiment_name"]}/git_config", "w+") as f:
+        f.write(" ".join(sys.argv[1:]))
+        
     # Race!
     runner.run()
