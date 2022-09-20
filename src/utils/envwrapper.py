1a9bc6f9f5c8ecbc782b071135f74dd85c7af63f
Arav Agarwal <arava@andrew.cmu.edu>
2022-09-19 21:53:13 -0400

    bugfix

--- src/utils/envwrapper.py
+++ src/utils/envwrapper.py
@@ -13,7 +13,7 @@ class EnvContainer:
     def _process_obs(self,obs:dict):
         obs_camera = obs['images']['CameraFrontRGB']
         obs_encoded = self.encoder.encode(obs_camera).to(DEVICE)
-        speed = torch.tensor(np.linalg.norm(obs['pose'][3:6], ord=2)).to(DEVICE)
+        speed = torch.tensor(np.linalg.norm(obs['pose'][3:6], ord=2)).to(DEVICE).reshape((-1,1))
         return torch.cat((obs_encoded, speed), 1).to(DEVICE)
 
     def step(self, action, env=None):
