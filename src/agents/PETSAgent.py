d964bf8cb0dbc76730660411ff5a530d2c5e5999
Arav Agarwal <arava@andrew.cmu.edu>
2022-10-01 16:16:41 -0400

    Add model_save_path

--- src/agents/PETSAgent.py
+++ src/agents/PETSAgent.py
@@ -28,7 +28,7 @@ import torch.nn.functional as F
 class PETSAgent(BaseAgent):
     """Adopted from https://github.com/BY571/PETS-MPC. Currently not using CEM, but random AS."""
 
-    def __init__(self, network_config_path: str, n_planner: int = 500, horizon: int = 12, lr: float = 1e-2):
+    def __init__(self, network_config_path: str, n_planner: int = 500, horizon: int = 12, lr: float = 1e-2, model_save_path: str = '/mnt/blah'):
         super().__init__()
         self.model = DynamicsNetwork.instantiate_from_config(network_config_path)
         self.n_planner = n_planner
