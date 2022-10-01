e28c6837c2758cfe9da516e2a77dc9ba7d824fcf
Arav Agarwal <arava@andrew.cmu.edu>
2022-10-01 16:33:36 -0400

    bugfix 4

--- src/agents/PETSAgent.py
+++ src/agents/PETSAgent.py
@@ -40,9 +40,7 @@ class PETSAgent(BaseAgent):
         
 
     def select_action(self, obs, noise=False) -> np.array:
-        obs = obs.detach().numpy()
-        state = torch.from_numpy(obs[None, :]).float()
-        print(state.shape, obs.shape)
+        state = obs.detach().float()
         initial_states = state.repeat((self.n_planner, 1)).to(self.device)
         rollout_actions =  torch.from_numpy(np.random.uniform(low=self.action_space.low,
                                     high=self.action_space.high,
