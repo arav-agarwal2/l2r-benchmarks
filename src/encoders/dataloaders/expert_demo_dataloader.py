f6b5a7876b1482f162f68d49acdf057073e1b32a
kchian <chian.kevin@gmail.com>
2022-10-09 14:49:18 -0400

    Update expert_demo_dataloader.py

--- src/encoders/dataloaders/expert_demo_dataloader.py
+++ src/encoders/dataloaders/expert_demo_dataloader.py
@@ -33,7 +33,7 @@ class ExpertDemoDataset(torch.utils.data.Dataset):
 
 @yamlize
 class ExpertDemoDataFetcher(BaseDataFetcher):
-    def __init__(self, train_path, val_path ):
+    def __init__(self, train_path: str, val_path: str):
         super(ExpertDemoDataFetcher, self).__init__()
         self.train_path = train_path
         self.val_path = val_path
