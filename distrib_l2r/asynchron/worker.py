f6b69b0de64eaaca68474e153aed4e55f6e809b8
Arav Agarwal <arava@andrew.cmu.edu>
2022-10-13 23:16:38 -0400

    bugfix

--- distrib_l2r/asynchron/worker.py
+++ distrib_l2r/asynchron/worker.py
@@ -172,7 +172,7 @@ class AsnycWorker:
         logging.warn(f"[is_train={is_train}] Collecting data")
         buffer = ReplayBuffer(size=self.buffer_size)
         collector = Collector(
-            policy=self.policy, env=self.env, buffer=buffer, exploration_noise=is_train
+            policy=policy, env=self.env, buffer=buffer, exploration_noise=is_train
         )
         result = collector.collect(n_episode=1)
         logging.warn(f"reward: {result['rew']}")
