286e07e42a2e855ad837535c7d3fb513be453501
Arav Agarwal <arava@andrew.cmu.edu>
2022-10-14 00:23:58 -0400

    Something something

--- server.py
+++ server.py
@@ -31,6 +31,8 @@ optim = torch.optim.Adam(net.parameters(), lr=1e-3)
 if __name__ == '__main__':
     learner = AsyncLearningNode(policy=DQNPolicy(model=net, optim=optim))
     print("Initialized!!.")
-    server_thread = threading.Thread(target=learner.learn)
-    print("Run foreva!")
-    learner.serve_forever()
+    server_thread = threading.Thread(target=learner.serve_forever)
+    server_thread.start()
+    print("Learning?")
+    while True:
+        learner.learn()
