6b47e2cde9cc69344a0b93ec789ec3f98eae250a
Arav Agarwal <arava@andrew.cmu.edu>
2022-10-14 00:21:30 -0400

    pika.

--- server.py
+++ server.py
@@ -31,5 +31,6 @@ optim = torch.optim.Adam(net.parameters(), lr=1e-3)
 if __name__ == '__main__':
     learner = AsyncLearningNode(policy=DQNPolicy(model=net, optim=optim))
     print("Initialized!!.")
-    server_thread = threading.Thread(target=learner.serve_forever)
-    learner.learn()
+    server_thread = threading.Thread(target=learner.learn)
+    print("Run foreva!")
+    learner.serve_forever()
