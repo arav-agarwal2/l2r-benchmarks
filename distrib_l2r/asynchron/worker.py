bea2334e1d138eb9a3b785cffb037264606e628b
Arav Agarwal <arava@andrew.cmu.edu>
2022-10-25 16:15:49 -0400

    Make temp change to l2r env

--- distrib_l2r/asynchron/worker.py
+++ distrib_l2r/asynchron/worker.py
@@ -19,89 +19,11 @@ from distrib_l2r.api import EvalResultsMsg
 from distrib_l2r.api import InitMsg
 from distrib_l2r.utils import send_data
 
-
-class EnvConfig(object):
-    multimodal = True
-    eval_mode = True
-    n_eval_laps = 1
-    max_timesteps = 5000
-    obs_delay = 0.1
-    not_moving_timeout = 100
-    reward_pol = "custom"
-    provide_waypoints = False
-    reward_kwargs = {
-        "oob_penalty": 5.0,
-        "min_oob_penalty": 25.0,
-        "max_oob_penalty": 125.0,
-    }
-    controller_kwargs = {
-        "sim_version": "ArrivalSim-linux-0.7.1.188691",
-        "quiet": False,
-        "user": "ubuntu",
-        "start_container": False,
-        "sim_path": "/home/LinuxNoEditor",
-    }
-    action_if_kwargs = {
-        "max_accel": 6,
-        "min_accel": -16,
-        "max_steer": .3,
-        "min_steer": -.3,
-        "ip": "0.0.0.0",
-        "port": 7077,
-    }
-    pose_if_kwargs = {
-        "ip": "0.0.0.0",
-        "port": 7078,
-    }
-    camera_if_kwargs = {
-        "ip": "0.0.0.0",
-        "port": 8008,
-    }
-    segm_if_kwargs = {
-        "ip": 'tcp://127.0.0.1',
-        "port": 8009
-    }
-    birdseye_if_kwargs = {
-        "ip": 'tcp://127.0.0.1',
-        "port": 8010
-    }
-    birdseye_segm_if_kwargs = {
-        "ip": 'tcp://127.0.0.1',
-        "port": 8011
-    }
-    logger_kwargs = {
-        "default": True,
-    }
-    cameras = {
-        "CameraFrontRGB": {
-            "Addr": "tcp://0.0.0.0:8008",
-            "Format": "ColorBGR8",
-            "FOVAngle": 90,
-            "Width": 512,
-            "Height": 384,
-            "bAutoAdvertise": True,
-        }
-    }
+# pip install git+https://github.com/learn-to-race/l2r.git@aicrowd-environment
+from l2r import build_env
+from l2r import RacingEnv
 
 
-class SimulatorConfig(object):
-    racetrack = "Thruxton"
-    active_sensors = [
-        "CameraFrontRGB",
-        "ImuOxtsSensor",
-    ]
-    driver_params = {
-        "DriverAPIClass": "VApiUdp",
-        "DriverAPI_UDP_SendAddress": "0.0.0.0",
-    }
-    camera_params = {
-        "Format": "ColorBGR8",
-        "FOVAngle": 90,
-        "Width": 512,
-        "Height": 384,
-        "bAutoAdvertise": True,
-    }
-    vehicle_params = False
 
 
 
@@ -134,7 +56,7 @@ class AsnycWorker:
         #sim_config = SimulatorConfig
         #self.env = RacingEnv(env_config.__dict__, sim_config.__dict__)
         #self.env.make()
-        self.env = gym.make('CartPole-v0')
+        self.env = build_env(controller_kwargs={"quiet": True})
 
 
         if env_wrapper:
