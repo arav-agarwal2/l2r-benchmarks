e1360188993ee647b6cd2b4e2f2f9547d82a5234
Arav Agarwal <arava@andrew.cmu.edu>
2022-10-25 18:21:49 -0400

    theoretically

--- distrib_l2r/asynchron/worker.py
+++ distrib_l2r/asynchron/worker.py
@@ -95,7 +95,7 @@ class AsnycWorker:
         self.encoder.to(DEVICE)
 
         #self.env.action_space = gym.spaces.Box(np.array([-0.3, -1]), np.array([0.3, 6]))
-        self.env = self.DummyVectorEnv([EnvContainer(self.encoder, self.env)])
+        self.env = DummyVectorEnv([EnvContainer(self.encoder, self.env)])
         
         #print(self.env.action_space)
     def work(self) -> None:
