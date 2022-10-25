7ceb3944e6812b9b0fe978e89e5c8ee68be108ff
Arav Agarwal <arava@andrew.cmu.edu>
2022-10-25 16:25:10 -0400

    bugfix

--- distrib_l2r/asynchron/worker.py
+++ distrib_l2r/asynchron/worker.py
@@ -62,7 +62,31 @@ class AsnycWorker:
             stdout=subprocess.DEVNULL,
         )
 
-        self.env = build_env(controller_kwargs={"quiet": True})
+        env = build_env(controller_kwargs={"quiet": True},
+            env_kwargs=
+                    {
+                        "multimodal": True,
+                        "eval_mode": True,
+                        "n_eval_laps": 5,
+                        "max_timesteps": 5000,
+                        "obs_delay": 0.1,
+                        "not_moving_timeout": 50000,
+                        "reward_pol": "custom",
+                        "provide_waypoints": False,
+                        "active_sensors": [
+                            "CameraFrontRGB"
+                        ],
+                        "vehicle_params":False,
+                    },
+            action_cfg=
+                    {
+                        "ip": "0.0.0.0",
+                        "port": 7077,
+                        "max_steer": 0.3,
+                        "min_steer": -0.3,
+                        "max_accel": 6,
+                        "min_accel": -1,
+                    })
 
 
         if env_wrapper:
