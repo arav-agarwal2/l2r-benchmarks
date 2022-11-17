9f1e9305c625304d63a3a7cfdc2d35aa8535250e
arav-agarwal2 <43820556+arav-agarwal2@users.noreply.github.com>
2022-11-17 14:51:20 -0500

    Update learner.py

--- distrib_l2r/asynchron/learner.py
+++ distrib_l2r/asynchron/learner.py
@@ -167,7 +167,7 @@ class AsyncLearningNode(socketserver.ThreadingMixIn, socketserver.TCPServer):
         """The thread where thread-safe gradient updates occur"""
         for epoch in tqdm(range(self.epochs)):
             semibuffer = self.buffer_queue.get()
-            print(f"Received something {len(semibuffer)}")
+            print(f"Received something {len(semibuffer)} vs {len(self.replay_buffer)}")
             # Add new data to the primary replay buffer
             self.replay_buffer.store(semibuffer)
 
