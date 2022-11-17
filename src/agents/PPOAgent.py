cde2827cd1ccc15ae2985399797c0d6327898805
Sidharth Kathpal <33653824+sidharthkathpal@users.noreply.github.com>
2022-11-17 14:15:05 -0500

    continued Tanays fixes

--- src/agents/PPOAgent.py
+++ src/agents/PPOAgent.py
@@ -20,7 +20,7 @@ from src.constants import DEVICE
 from src.config.parser import read_config
 from src.config.schema import agent_schema
 
-from src.utils.envwrapper_aicrowd import EnvContainer
+# from src.utils.envwrapper_aicrowd import EnvContainer
 
 
 @yamlize
--- src/agents/SACAgent.py
+++ src/agents/SACAgent.py
@@ -47,8 +47,6 @@ class SACAgent(BaseAgent):
         load_checkpoint: bool,
         model_save_path: str,
         lr: float,
-        update_lr_every: int,
-        lr_update_rate: float,
     ):
         super(SACAgent, self).__init__()
 
@@ -64,8 +62,6 @@ class SACAgent(BaseAgent):
         self.load_checkpoint = load_checkpoint
         self.model_save_path = model_save_path
         self.lr = lr
-        self.update_lr_every = None
-        self.lr_update_rate = lr_update_rate
 
         self.save_episodes = True
         self.episode_num = 0
@@ -287,19 +283,6 @@ class SACAgent(BaseAgent):
             "metadata": info,
         }
         return self.recording
-
-    def update_lr(self, episode=None):
-        if self.update_lr_every is None or self.lr_update_rate is None:
-            self.lr = self.lr
-        else:
-            # if(episode//100%2 == 1):
-            #    if(episode%self.update_lr_every == 0):
-            #        self.lr *= (1 - self.lr_update_rate)
-            # else:
-            #    if(episode%self.update_lr_every == 0):
-            #        self.lr *= (1 + self.lr_update_rate)
-            if episode % self.update_lr_every == 0:
-                self.lr *= 1 - self.lr_update_rate
 
     """def log_val_metrics_to_tensorboard(self, info, ep_ret, n_eps, n_val_steps):
         self.tb_logger.add_scalar("val/episodic_return", ep_ret, n_eps)
