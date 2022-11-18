b85ef7547cbed20551a3b41755cdd0af638f138f
Arav Agarwal <aravagar@umich.edu>
2022-11-18 04:12:06 -0500

    log

--- src/runners/WorkerRunner.py
+++ src/runners/WorkerRunner.py
@@ -52,6 +52,7 @@ class WorkerRunner(BaseRunner):
             t += 1
             self.agent.deterministic = not is_train
             action_obj = self.agent.select_action(state_encoded)
+            print(action_obj.action)
             next_state_encoded, reward, done, terminated, _= env.step(action_obj.action)
             # print(f'info{info}')
             state_encoded, next_state_encoded = torch.Tensor(state_encoded), torch.Tensor(next_state_encoded)
