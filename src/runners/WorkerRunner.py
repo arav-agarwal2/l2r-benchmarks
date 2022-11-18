9838a093d38bd6ab325ce368ea63252b0ceead28
Arav Agarwal <aravagar@umich.edu>
2022-11-18 03:51:53 -0500

    print help

--- src/runners/WorkerRunner.py
+++ src/runners/WorkerRunner.py
@@ -50,6 +50,7 @@ class WorkerRunner(BaseRunner):
             t += 1
             self.agent.deterministic = not is_train
             action_obj = self.agent.select_action(state_encoded)
+            print(env.step(action_obj.action))
             next_state_encoded, reward, done, _= env.step(action_obj.action)
             # print(f'info{info}')
             ep_ret += reward
