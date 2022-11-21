030ad0dc7925393a9538c31f63cabe9b948d8f69
Arav Agarwal <aravagar@umich.edu>
2022-11-20 21:34:40 -0500

    Noticing that multithreading seems to be an issue. Trying a pool.

--- distrib_l2r/asynchron/learner.py
+++ distrib_l2r/asynchron/learner.py
@@ -2,6 +2,7 @@ import logging
 import queue
 import random
 import socketserver
+import threading
 from typing import Any
 from typing import Callable
 from typing import Dict
@@ -9,6 +10,7 @@ from typing import Optional
 from typing import Tuple
 import time
 from tqdm import tqdm
+import sys
 import socket
 from src.agents.base import BaseAgent
 from src.config.yamlize import create_configurable, NameToSourcePath, yamlize
@@ -22,6 +24,66 @@ from distrib_l2r.api import PolicyMsg
 from distrib_l2r.utils import receive_data
 from distrib_l2r.utils import send_data
 
+
+class ThreadPoolMixin:
+    """
+    Mixin to use a fixed pool of threads to handle requests.
+    .. note::
+       When shutting down the server, please ensure you call this mixin's
+       `join()` to shut down the pool along with the server's `shutdown()`
+       method. The order in which these are performed is not significant,
+       but both actions must be performed.
+    """
+
+    # Size of pool.
+    pool_size = 5
+
+    # How long to wait on an empty queue, in seconds. Can be a float.
+    timeout_on_get = 0.5
+
+    def __init__(self):
+        self._request_queue = queue.Queue(self.pool_size)
+        # This beastie serves a different purpose than __shutdown_request
+        # and __is_shut_down: those are superprivate so we can't touch them,
+        # and even if we could, they're not really useful in shutting down
+        # the pool.
+        self._shutdown_event = threading.Event()
+        for _ in range(self.pool_size):
+            thread = threading.Thread(target=self.process_request_thread)
+            thread.setDaemon(1)
+            thread.start()
+
+    def process_request_thread(self):
+        """Same as in BaseServer, but as a thread."""
+        while True:
+            try:
+                request, client_address = self._request_queue.get(
+                    timeout=self.timeout_on_get,
+                )
+            except queue.Empty:
+                if self._shutdown_event.isSet():
+                    return
+                continue
+            try:
+                self.finish_request(request, client_address)
+                self.shutdown_request(request)
+            except:
+                self.handle_error(request, client_address)
+                self.shutdown_request(request)
+            self._request_queue.task_done()
+
+    def process_request(self, request, client_address):
+        """Queue the given request."""
+        self._request_queue.put((request, client_address))
+
+    def join(self):
+        """Wait on the pool to clear and shut down the worker threads."""
+        # A nicer place for this would be shutdown(), but this being a mixin,
+        # that method can't safely do anything with that method, thus we add
+        # an extra method explicitly for clearing the queue and shutting
+        # down the workers.
+        self._request_queue.join()
+        self._shutdown_event.set()
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
     """Request handler thread created for every request"""
@@ -59,7 +121,7 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 
 
 
-class AsyncLearningNode(socketserver.ThreadingMixIn, socketserver.TCPServer):
+class AsyncLearningNode(ThreadPoolMixin, socketserver.TCPServer):
     """A multi-threaded, offline, off-policy reinforcement learning server
 
     Args:
