7f1ccfa673be744dcff1bd5ada950703ccb825dc
tanay-gangey <tanay.gangey@gmail.com>
2022-11-22 21:22:15 -0500

    Revert SDE changes

--- src/networks/network_baselines.py
+++ src/networks/network_baselines.py
@@ -40,14 +40,13 @@ class SquashedGaussianMLPActor(nn.Module):
         super().__init__()
         self.net = mlp([obs_dim] + list(hidden_sizes), activation, activation)
         self.mu_layer = nn.Linear(hidden_sizes[-1], act_dim)
-        self.log_std_layer = nn.Parameter(torch.ones(act_dim) *(-3), requires_grad=True)
+        self.log_std_layer = nn.Linear(hidden_sizes[-1], act_dim)
         self.act_limit = act_limit
 
     
     def forward(self, obs, deterministic=False, with_logprob=True):
         net_out = self.net(obs)
-        mu_layer = nn.Sequential(self.mu_layer, nn.Hardtanh(min_val=-2.0, max_val=2.0))
-        mu = mu_layer(net_out)
+        mu = self.mu_layer(net_out)
         log_std = self.log_std_layer(net_out)
         log_std = torch.clamp(log_std, LOG_STD_MIN, LOG_STD_MAX)
         std = torch.exp(log_std)
@@ -71,8 +70,6 @@ class SquashedGaussianMLPActor(nn.Module):
             #logp_pi -= (2 * (np.log(2) - pi_action - F.softplus(-2 * pi_action))).sum(
             #    axis=1
             #)
-
-
             var = std**2
             log_scale = torch.log(std)
             # Attempt at speeding up logprob calculation. torch uses math, which seems to be slow.
