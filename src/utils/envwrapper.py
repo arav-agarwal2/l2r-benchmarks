bdc13ff9c4febfd80605979445c7f5118565d781
Sidharth Kathpal <33653824+sidharthkathpal@users.noreply.github.com>
2022-12-03 16:20:29 -0500

    removing the comments and the import logging statement

--- src/utils/envwrapper.py
+++ src/utils/envwrapper.py
@@ -3,7 +3,6 @@ import numpy as np
 import torch
 import itertools
 from src.constants import DEVICE
-import logging
 
 
 class EnvContainer:
@@ -30,8 +29,6 @@ class EnvContainer:
         assert np.any(
             obs_camera != 0
         ), "Camera obs should have a value != 0 (black screen)"
-        # if np.all(obs_camera==0):
-        #     logging.info("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
         obs_encoded = self.encoder.encode(obs_camera).to(DEVICE)
         speed = (
             torch.tensor(np.linalg.norm(obs["pose"][3:6], ord=2))
